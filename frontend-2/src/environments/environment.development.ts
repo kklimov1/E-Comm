0dedbc5df1be18c1575baa76b74b24f355a30016
kklimov1 <kirillvlklimov@gmail.com>
2023-04-07 11:05:54 -0500

    Updated developer environment

--- frontend-2/src/environments/environment.development.ts
+++ frontend-2/src/environments/environment.development.ts
@@ -1,9 +1,12 @@
+
 export const environment = {
-        production: false,
-        withCredentials: true,
-        baseUrl: "http://localhost:5432",
-        headers: {
+    production: true,
+    withCredentials: true,
+    baseUrl: "http://ec2-54-87-34-154.compute-1.amazonaws.com:8080",
+    headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://localhost:4200',
-    },
+        'Access-Control-Allow-Origin': 'http://statichostingmysite.s3-website-us-east-1.amazonaws.com',
+        'Origin': 'http://ec2-54-87-34-154.compute-1.amazonaws.com:8080',
+        'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
+      }
 };
