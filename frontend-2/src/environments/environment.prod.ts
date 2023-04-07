3680871525a300ab76d3cfd422cd7b7ad22aee31
kklimov1 <kirillvlklimov@gmail.com>
2023-04-07 10:40:39 -0500

    Added production port base url and output

--- frontend-2/src/environments/environment.prod.ts
+++ frontend-2/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-    production: true
-  };
+    production: true,
+    withCredentials: true,
+    baseUrl: "http://ec2-54-87-34-154.compute-1.amazonaws.com:8080",
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://statichostingmysite.s3-website-us-east-1.amazonaws.com',
+        'Origin': 'http://ec2-54-87-34-154.compute-1.amazonaws.com:8080',
+        'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
+      }
+};
