8e28875de63e67dcc05e07434ad535a685fd7ef7
kklimov1 <kirillvlklimov@gmail.com>
2023-04-07 10:44:41 -0500

    Altered base environment with port ec2

--- frontend-2/src/environments/environment.ts
+++ frontend-2/src/environments/environment.ts
@@ -1,4 +1,15 @@
 export const environment = {
+    production: true,
+    withCredentials: true,
+    baseUrl: "http://ec2-54-87-34-154.compute-1.amazonaws.com:8080",
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://statichostingmysite.s3-website-us-east-1.amazonaws.com',
+        'Origin': 'http://ec2-54-87-34-154.compute-1.amazonaws.com:8080',
+        'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
+      }
+};
+/*export const environment = {
     production: false,
     withCredentials: true,
     baseUrl: "http://localhost:5432",
@@ -6,4 +17,4 @@ export const environment = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:4200',
 },
-};
+};*/
